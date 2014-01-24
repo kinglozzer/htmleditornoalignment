84c01ae5688e6d6c5ed3de770d5a003045ddf713
Loz Calver <lozcalver@bigfork.co.uk>
2014-01-24 14:51:10 +0000

    Initial commit

---
+++ code/HtmlEditorField_NoAlignmentExtension.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Add 'no alignment' option when inserting images through 'Insert Media' form
+ */
+class HtmlEditorField_NoAlignmentExtension extends Extension {
+
+	public function updateFieldsForImage(&$fields, $url, $file) {
+		$classNameField = $fields->fieldByName('CSSClass');
+		$classes = $classNameField->getSource();
+		$classes = array_merge(array('' => _t('HtmlEditorField.CSSCLASSNONE', 'No alignment.')), $classes);
+		$classNameField->setSource($classes);
+		return $fields;
+	}
+
+}
